aa4f5d617a458d6ad549955fadaca0a845f16beb
MagKon <magnus.kongstad@gmail.com>
2023-04-17 11:08:53 +0200

    Revert "Added shapes"
    This reverts commit 80b2fcdf2565e96557a286ab277a760ada90f51b.

--- src/main/java/TicTacToe/Shapes/Shapes.java
+++
@@ -1,9 +0,0 @@
-package TicTacToe.Shapes;
-
-import org.abstractica.javacsg.Geometry;
-import org.abstractica.javacsg.Geometry3D;
-
-public interface Shapes {
-    Geometry getGeometry();
-    Geometry setGeometry(Geometry geometry);
-}
--- src/main/java/TicTacToe/Shapes/X.java
+++ src/main/java/TicTacToe/Shapes/X.java
@@ -1,16 +1,7 @@
 package TicTacToe.Shapes;
 
-import org.abstractica.javacsg.Geometry;
 import org.abstractica.javacsg.Geometry2D;
-import org.abstractica.javacsg.Geometry3D;
-import org.abstractica.javacsg.JavaCSG;
 
 public class X {
     Geometry2D geometry;
-    JavaCSG csg;
-
-
-    public X(JavaCSG csg) {
-        this.csg = csg;
-    }
 }
