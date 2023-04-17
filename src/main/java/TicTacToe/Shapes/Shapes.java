80b2fcdf2565e96557a286ab277a760ada90f51b
MagKon <magnus.kongstad@gmail.com>
2023-04-17 10:18:39 +0200

    Added shapes

---
+++ src/main/java/TicTacToe/Shapes/Shapes.java
@@ -0,0 +1,9 @@
+package TicTacToe.Shapes;
+
+import org.abstractica.javacsg.Geometry;
+import org.abstractica.javacsg.Geometry3D;
+
+public interface Shapes {
+    Geometry getGeometry();
+    Geometry setGeometry(Geometry geometry);
+}
