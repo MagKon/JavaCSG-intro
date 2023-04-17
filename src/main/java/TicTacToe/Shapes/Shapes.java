a9a29d4b36659c5d6c0218972be70ce1fef6aff4
MagKon <magnus.kongstad@gmail.com>
2023-04-17 11:08:41 +0200

    Revert "Update Shapes.java"
    This reverts commit cf47c53ea8c95b6d3e1229cf533a10680555ccad.

--- src/main/java/TicTacToe/Shapes/Shapes.java
+++ src/main/java/TicTacToe/Shapes/Shapes.java
@@ -5,5 +5,5 @@ import org.abstractica.javacsg.Geometry3D;
 
 public interface Shapes {
     Geometry getGeometry();
-    void setGeometry(Geometry geometry);
+    Geometry setGeometry(Geometry geometry);
 }
