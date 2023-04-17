8e69bccdfe085bf5969029233ced36827ffcf192
MagKon <magnus.kongstad@gmail.com>
2023-04-17 11:08:30 +0200

    Revert "Added translate function"
    This reverts commit 2d8982b9b131570f17ea6544c26fd68338396a7f.

--- src/main/java/TicTacToe/Shapes/Shapes.java
+++ src/main/java/TicTacToe/Shapes/Shapes.java
@@ -9,5 +9,4 @@ public interface Shapes {
     void setGeometry2D(Geometry2D geometry);
     Geometry3D getGeometry3D();
     void setGeometry3D(Geometry3D geometry);
-    void translate(double x, double y, double z);
 }
