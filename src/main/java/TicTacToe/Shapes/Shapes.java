2d8982b9b131570f17ea6544c26fd68338396a7f
MagKon <magnus.kongstad@gmail.com>
2023-04-17 10:30:04 +0200

    Added translate function

--- src/main/java/TicTacToe/Shapes/Shapes.java
+++ src/main/java/TicTacToe/Shapes/Shapes.java
@@ -9,4 +9,5 @@ public interface Shapes {
     void setGeometry2D(Geometry2D geometry);
     Geometry3D getGeometry3D();
     void setGeometry3D(Geometry3D geometry);
+    void translate(double x, double y, double z);
 }
